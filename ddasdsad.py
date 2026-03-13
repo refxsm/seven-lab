1790f27fb586e8d65674e31f6f25c457a4ea765b
refxsm <artemii.ok2019@gmail.com>
2026-03-13 14:42:22 +0300

    Update ddasdsad.py

--- ddasdsad.py
+++ ddasdsad.py
@@ -2,6 +2,4 @@ Python 3.14.0a4 (tags/v3.14.0a4:f26daa9, Jan 14 2025, 13:27:29) [MSC v.1942 64 b
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print: "HelloWorld"
 >>> 231331
-231331
->>> 333
-333
+fdsasdsadlajdsklajdsajdsa;ljdl;AMD64

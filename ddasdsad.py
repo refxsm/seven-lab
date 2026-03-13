6e421b5924dfdd1a865af64c1e8ba9e424003704
refxsm <artemii.ok2019@gmail.com>
2026-03-13 14:40:13 +0300

    Add files via upload

---
+++ ddasdsad.py
@@ -0,0 +1,7 @@
+Python 3.14.0a4 (tags/v3.14.0a4:f26daa9, Jan 14 2025, 13:27:29) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print: "HelloWorld"
+>>> 231331
+231331
+>>> 333
+333
